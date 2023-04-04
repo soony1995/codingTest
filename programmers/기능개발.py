c3dec66bd0cfa37e27da1c3f53a69b06d1d9c20e
soony1995 <59558831+soony1995@users.noreply.github.com>
2023-04-04 17:29:32 +0900

    Update 기능개발.py

--- programmers/기능개발.py
+++ programmers/기능개발.py
@@ -1,15 +1,23 @@
 def solution(progresses, speeds):
 
-    stack = []
+    answer = []
+    time = 0
+    count = 0
     
-    k = 1
-    
-    while k<len(speeds):
-        for i in speeds:
-            print(i,k)
-            progresses[k] += i * k 
-            print(progresses[k])
-        
-        k += 1
-        
-    return 
+    while len(progresses)> 0:
+        if (progresses[0] + time*speeds[0]) >= 100: 
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+            
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer
+
+// 처음 인자가 100이 되는 시점에 그 이후 인덱스를 검사해 만약에 100이 된게 잇다면 전부 cnt를 늘리고 pop을 할 생각이였음.
+// 이렇게 생각하니 복잡해 로직을 짜는데 어려움이 생김.
+// 그냥 간단하게 하나씩 하면 되는걸..
